96dd2c9b46f4f6dc7305d3361f021077e556c3f4
Psyhackological <konradkon@duck.com>
2022-11-10 18:43:33 +0100

    Something is working!

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-use svg::node::element::path::{Command, Data};
-use svg::node::element::tag::Path;
 use svg::parser::Event;
 
 fn main() {
-    let path = "svg/rust-logo-blk.svg";
+    let path = "svg/rust_logo_black.svg";
     let mut content = String::new();
     for event in svg::open(path, &mut content).unwrap() {
-        if let Event::Tag(Path, _, attributes) = event {
-            let data = attributes.get("d").unwrap();
-            let data = Data::parse(data).unwrap();
-            for command in data.iter() {
-                match command {
-                    Command::Move(..) => println!("Move!"),
-                    Command::Line(..) => println!("Line!"),
-                    Command::HorizontalLine(..) => println!("Horizontal Line!"),
-                    Command::VerticalLine(..) => println!("Vertical Line!"),
-                    Command::QuadraticCurve(..) => println!("Quadratic Curve!"),
-                    Command::SmoothQuadraticCurve(..) => println!("Smooth Quadratic Curve!"),
-                    Command::CubicCurve(..) => println!("Cubic Curve!"),
-                    Command::SmoothCubicCurve(..) => println!("Smooth Cubic Curve!"),
-                    Command::EllipticalArc(..) => println!("Elliptical Arc!"),
-                    Command::Close => println!("Close!"),
+        match event {
+            Event::Tag(tag_name, _,ref hm) => {
+                if let Some(value) = hm.get("fill") {
+                    println!("{:?} {:?}", tag_name, value);
+                }
+                if let Some(value) = hm.get("stroke") {
+                    println!("{:?} {:?}", tag_name, value);
                 }
             }
+            _ => {},
         }
     }
 }
+
