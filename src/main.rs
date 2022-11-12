fe0af4193ddb0b8bbbe55ada8c580b55e1efa6c6
Psyhackological <konradkon@duck.com>
2022-11-12 17:10:15 +0100

    Searching for more ways to check for a fill and stroke

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use svg::parser::Event;
 
 fn main() {
-    let path = "svg/rust_logo_black.svg";
+    let path = "svg/lichess_logo_black.svg";
     let mut content = String::new();
-    for event in svg::open(path, &mut content).unwrap() {
+    for event in svg::open(path, &mut content).expect("The file in the given path does not exist!")
+    {
         if let Event::Tag(tag_name, _, ref hm) = event {
+            if tag_name == "style" {
+                println!("{:?}", event);
+            }
             if let Some(value) = hm.get("fill") {
                 println!("{:?} {:?}", tag_name, value);
             }
             if let Some(value) = hm.get("stroke") {
+                println!("{:?} {:?}", tag_name, value);
+            }
+            if let Some(value) = hm.get("style") {
                 println!("{:?} {:?}", tag_name, value);
             }
         }
